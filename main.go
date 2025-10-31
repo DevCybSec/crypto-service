5022b241257a5a120ef50b9c2b3487a42f115d3c
devcybsec <edgaremp007@icloud.com>
2025-10-31 14:13:02 -0600

    Fix: Allow credential was added to cors.

--- main.go
+++ main.go
@@ -483,6 +483,8 @@ func corsMiddleware(next http.Handler) http.Handler {
 		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+		
 		if r.Method == "OPTIONS" {
 			w.WriteHeader(http.StatusOK)
 			return
